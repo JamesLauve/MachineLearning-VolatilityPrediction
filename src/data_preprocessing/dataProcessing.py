86fb4017422dba19e1e6164b5f132a12a5e677c7
James <lauvige.james@gmail.com>
2025-12-04 11:43:11 +0100

    Refactor data processing functions for improved structure and readability

--- src/data_preprocessing/dataProcessing.py
+++ src/data_preprocessing/dataProcessing.py
@@ -1,15 +1,38 @@
 import dataLoader.py as dl  
 import numpy as np
+import matplotlib.pyplot as plt
 
 sp500 = dl.load_stock_data()
+def SetReturns(data):
+    
+    #let's print missing values
+    print(data.isnull().sum())
 
-#let's print missing values
-print(sp500.isnull().sum())
+    # Basic data cleaning: Fill missing values with forward fill method
+    data.fillna(method='ffill', inplace=True)
 
-# Basic data cleaning: Fill missing values with forward fill method
-sp500.fillna(method='ffill', inplace=True)
+    data['Return'] = data['Close'].pct_change()
+    data['LogReturn'] = np.log(data['Close'] / data['Close'].shift(1))
 
-sp500['Return'] = sp500['Close'].pct_change()
-sp500['LogReturn'] = np.log(sp500['Close'] / sp500['Close'].shift(1))
+    #Let's observe our data information after processing
+    print('Statistical Summary after Processing:')
+    print(sp500.describe())
+
+def plotting_SP(data):
+    plt.figure(figsize=(12, 5))
+    plt.plot(data.index, data["Close"])
+    plt.title("S&P 500 - Close")
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.show()
+
+def plotting_Histo_Returns(data):
+    plt.figure(figsize=(10, 4))
+    plt.hist(data["Returns"].dropna(), bins=100)
+    plt.title("Histogram of Daily Log Returns")
+    plt.xlabel("Returns")
+    plt.ylabel("Frequency")
+    plt.show()
+
 
 
